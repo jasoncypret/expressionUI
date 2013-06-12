36ef1b026204c479f65b47990dfdf2dffdab70c6
Jason Cypret <jason@jasoncypret.com>
2013-06-12 17:52:55 -0500

    Popover is damn near perfect // new options for fixed elements // close on scroll  //  reposition on resize | click to close "anywhere" & "outside" // tooltip flag makes arrow in the middle

--- lib/expressionui/version.rb
+++ lib/expressionui/version.rb
@@ -1,3 +1,3 @@
 module Expressionui
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 end
--- vendor/assets/javascripts/expressionui.popover.js
+++ vendor/assets/javascripts/expressionui.popover.js
@@ -11,7 +11,9 @@
       content: $('.popover'),
       padding: 5,
       flush: false,
+      fixed: false,
       close_on_scroll: false,
+      scroll_target: $(document),
       scroll_threshold: 5,
       close_on_click: "anywhere",
       reposition_on_resize: false,
@@ -28,41 +30,29 @@
       options = $.extend({}, methods.defaults, options);
 
       var outter_height, outter_width;
-      
       outter_height = ($(this).outerHeight() === 0) ? $(this).attr('height') : $(this).outerHeight() ;
       outter_width = ($(this).outerWidth() === 0) ? $(this).attr('width') : $(this).outerWidth() ;
       
       options.position_left = $(this).offset().left + (outter_width / 2);
       options.position_top = $(this).offset().top + (outter_height / 2);
 
-      // temp
-      options.arrow_position = null
-
       if(!options.arrow_position) options.arrow_position = $(this).popover('_positionArrow', options);
-      
-
 
       options.content.addClass('popover_container ' + options.arrow_position).css({visibility: 'hidden', display: 'block', zIndex: '9999'});
       if(options.flush) options.content.addClass('flush');
+      if(options.tooltip) options.content.addClass('tooltip');
+      if (options.fixed) options.content.css({'position': 'fixed' });
       
-
       var tooltipHeight = options.content.outerHeight(),
           tooltipWidth = options.content.outerWidth();
 
 
+      if (options.fixed) {
+        options.position_top += -$(options.scroll_target).scrollTop();
+        options.position_left += -$(options.scroll_target).scrollLeft();
+      }
+
       switch (options.arrow_position) {
-
-        // fixed position is different
-
-        // LEFT IS WORKING CORRECTLY...JUST NOT MIDDLE
-        // DOnt switch on tooltip
-          // top or bottom
-          // and always middle
-
-        // Flush should be calculated differently
-          // top: $(this).outerHeight() no padding
-          // left: options.position_left*2
-
         case "topleft":
           if (options.flush) {
             options.position_top = outter_height;
@@ -87,55 +77,73 @@
           break;
       }
 
-      console.log( 'left: ' + options.position_left + 'px\n' + 'top: ' + options.position_top + 'px\n' + 'tooltipHeight: ' + tooltipHeight + 'px\n' + 'tooltipWidth: ' + tooltipWidth + 'px\n' + 'arrow_position: ' + options.arrow_position + '\n' + 'arrow_offset: ' + options.arrow_offset + 'px\n');
+      if (options.tooltip) {
+        options.position_left = $(this).offset().left + (outter_width / 2) -(tooltipWidth/2);
+      }
 
+      // TODO: add an active_popover class to $(this)
       options.content.css({ 'left': options.position_left + 'px', 'top': options.position_top + 'px', 'display': 'none', 'visibility': 'visible' }).show();
-
       options.afterOpen.apply(this, [options.content])
 
-
-      var num = 0
-      // You could add an active_popover class to $(this)
-      $(document).bind('click.popover', function(e) {
-        num++;
-        if (num > 1) {
-          $(options.content).removeClass('popover_container topleft bottomleft topright bottomright').hide();
-          options.afterClose.apply(this, [options.content])
-          $(document).unbind('click.popover');
-        }
-      });
-
-      //$('body').bind('click', function(e) {
-      //if($(e.target).closest('#menu').length == 0) {
-      // click happened outside of menu, hide any visible menu items
-      //}
-      //});
-
-
-    },
-    _open: function (options) {
-      options.beforeOpen();
+      $(this).popover('_setupEvents', options);
     },
     close: function (options) {
       options = $.extend({}, methods.defaults, options);
-      options.beforeClose();
-      options.afterClose();
+      $(options.content).removeClass('popover_container topleft bottomleft topright bottomright tooltip flush').attr('style', '').hide();
+      $(this).popover('_destroyEvents', options);
+      options.afterClose.apply(this, [options.content]);
+    },
+    _destroyEvents: function (options) {
+      $(document).unbind('click.popover');
+      $(document).unbind('scroll.popover');
+      $(document).unbind('resize.popover');
     },
     _setupEvents: function (options) {
-      var _this = $(this)
+      var _this = $(this),
+          click = 0;
+
       if (options.reposition_on_resize) {
-        $(window).bind("reposition.popover", function () {
-          //_this.find('.modal').modal('position', options)
+        $(window).bind("resize.popover", function () {
+          // TODO:
+          //$(this).popover('_positionArrow', options);
+          //$(this).popover('_positionPopover', options);
         });
+      }
+
+      $(document).bind('click.popover', function(e) {
+        click++;
+        if (click > 1) {
+          $(this).popover('close', options);
+        }
+      });
+
+      if (options.close_on_scroll) {
+        $(options.scroll_target).bind('scroll.popover', function() {
+          $(document).trigger('content.scroll')
+          if ( $(options.scroll_target).scrollTop() >= options.scroll_threshold ){
+            $(this).popover('close', options);
+          }
+        });
+      }
+      switch (options.close_on_click) {
+        case "anywhere":
+          // no block
+          break;
+        case "outside":
+          $(options.content).click(function(e) {
+            e.stopPropagation();
+            return false;
+          });
+          break;
       }
     },
     _positionArrow: function (options) {
-      var winTopMax = $(window).height() / 2,
-          winLeftMax = $(window).width() / 2,
+      var winTopMax = $(options.scroll_target).height() / 2,
+          winLeftMax = $(options.scroll_target).width() / 2,
           pos = '';
 
-      (options.position_top >= 1 && options.position_top - $(window).scrollTop() <= winTopMax) ? pos = 'top' : pos = 'bottom';
-      (options.position_left >= 1 && options.position_left - $(window).scrollLeft() <= winLeftMax) ? pos += 'left' : pos += 'right';      
+      (options.position_top >= 1 && options.position_top - $(options.scroll_target).scrollTop() <= winTopMax) ? pos = 'top' : pos = 'bottom';
+      (options.position_left >= 1 && options.position_left - $(options.scroll_target).scrollLeft() <= winLeftMax) ? pos += 'left' : pos += 'right';      
       
       return pos;
     },
