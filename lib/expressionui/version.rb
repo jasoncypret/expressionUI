fb627a2b7de61b857bf54bcf5e9cefab6458cf4b
Jason Cypret <jason@jasoncypret.com>
2013-06-12 14:00:39 -0500

    Refactored popover | added more options and made it more modular | More options on next checkin

--- lib/expressionui/version.rb
+++ lib/expressionui/version.rb
@@ -1,3 +1,3 @@
 module Expressionui
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
--- vendor/assets/javascripts/expressionui.popover.js
+++ vendor/assets/javascripts/expressionui.popover.js
@@ -6,67 +6,149 @@
  */
 
 (function ($) {
-  $.fn.popover = function (options) {
-    var defaults = {
-        content: $('.popover'),
-        padding: 0,
-        aOffset: 55,
-        afterOpen: $.noop,
-        afterClose: $.noop,
-        flush: false
-    };
-    options = $.extend({}, defaults, options);
-    var left = $(this).offset().left + ($(this).outerWidth() / 2),
-      top = $(this).offset().top + ($(this).outerHeight() / 2),
-      winTopMax = $(window).height() / 2,
-      winLeftMax = $(window).width() / 2,
-      pos = '',
-      num = 0,
-      tooltipHeight,
-      tooltipWidth;
+  var methods = {
+    defaults: {
+      content: $('.popover'),
+      padding: 5,
+      flush: false,
+      close_on_scroll: false,
+      scroll_threshold: 5,
+      close_on_click: "anywhere",
+      reposition_on_resize: false,
+      tooltip: false,
+      arrow_height: 12,
+      arrow_offset: 35,
+      arrow_position: null,
+      position_top: '',
+      position_left: '',
+      afterOpen: $.noop,
+      afterClose: $.noop
+    },
+    init: function (options) {
+      options = $.extend({}, methods.defaults, options);
 
-    pos = options.position;
-    if(!pos){
-        (top >= 1 && top - $(window).scrollTop() <= winTopMax) ? pos = 'top' : pos = 'bottom';
-        (left >= 1 && left - $(window).scrollLeft() <= winLeftMax) ? pos += 'left' : pos += 'right';
-    }
+      var outter_height, outter_width;
+      
+      outter_height = ($(this).outerHeight() === 0) ? $(this).attr('height') : $(this).outerHeight() ;
+      outter_width = ($(this).outerWidth() === 0) ? $(this).attr('width') : $(this).outerWidth() ;
+      
+      options.position_left = $(this).offset().left + (outter_width / 2);
+      options.position_top = $(this).offset().top + (outter_height / 2);
+
+      // temp
+      options.arrow_position = null
+
+      if(!options.arrow_position) options.arrow_position = $(this).popover('_positionArrow', options);
+      
 
 
-    options.content.addClass('popover_container ' + pos).css({visibility: 'hidden', display: 'block', zIndex: '9999'});
-    if(options.flush) options.content.addClass('flush');
-    tooltipHeight = options.content.outerHeight();
-    tooltipWidth = options.content.outerWidth();
-    switch (pos) {
-      case "topleft":
-        top += ($(this).height() / (options.flush ? 2 : 1)) + options.padding * 2;
-        left += -$(this).width() / 2 - options.aOffset;
-        break;
-      case "bottomleft":
-        top += -tooltipHeight - $(this).height() / 2 - options.padding;
-        left += -$(this).width() / 2 - options.aOffset;
-        break;
-      case "topright":
-        top += $(this).height() + options.padding * 2;
-        left += $(this).width() - (tooltipWidth + (options.aOffset *2));
-        break;
-      case "bottomright":
-        top += -$(this).height() / 2 - tooltipHeight - options.padding;
-        left += $(this).width() - (tooltipWidth + (options.aOffset * 2));
-        break;
-    }
+      options.content.addClass('popover_container ' + options.arrow_position).css({visibility: 'hidden', display: 'block', zIndex: '9999'});
+      if(options.flush) options.content.addClass('flush');
+      
 
-    options.content.css({ 'left': left + 'px', 'top': top + 'px', 'display': 'none', 'visibility': 'visible' }).show();
+      var tooltipHeight = options.content.outerHeight(),
+          tooltipWidth = options.content.outerWidth();
 
-    options.afterOpen.apply(this, [options.content])
 
-    // You could add an active_popover class to $(this)
-    $(document).bind('click.popover', function(e) {
-      num++;
-      if (num > 1) {
-        $(options.content).removeClass('popover_container topleft bottomleft topright bottomright').hide();
-        options.afterClose.apply(this, [options.content])
-        $(document).unbind('click.popover');
+      switch (options.arrow_position) {
+
+        // fixed position is different
+
+        // LEFT IS WORKING CORRECTLY...JUST NOT MIDDLE
+        // DOnt switch on tooltip
+          // top or bottom
+          // and always middle
+
+        // Flush should be calculated differently
+          // top: $(this).outerHeight() no padding
+          // left: options.position_left*2
+
+        case "topleft":
+          if (options.flush) {
+            options.position_top = outter_height;
+            options.position_left += -(outter_width/2)
+          } else {
+            options.position_top += (outter_height/2) + options.arrow_height + options.padding;
+            options.position_left += -(options.arrow_offset);
+          }
+          break;
+        case "bottomleft":
+          options.position_top += -tooltipHeight -(outter_height/2) - options.arrow_height - options.padding;
+          options.position_left += -(options.arrow_offset);
+          break;
+        case "topright":
+          options.position_top += (outter_height/2) + options.arrow_height + options.padding;
+          options.position_left += -(tooltipWidth) + (options.arrow_offset)
+          break;
+
+        case "bottomright":
+          options.position_top += -tooltipHeight -(outter_height/2) - options.arrow_height - options.padding;
+          options.position_left += -(tooltipWidth) + (options.arrow_offset)
+          break;
       }
-    });
+
+      console.log( 'left: ' + options.position_left + 'px\n' + 'top: ' + options.position_top + 'px\n' + 'tooltipHeight: ' + tooltipHeight + 'px\n' + 'tooltipWidth: ' + tooltipWidth + 'px\n' + 'arrow_position: ' + options.arrow_position + '\n' + 'arrow_offset: ' + options.arrow_offset + 'px\n');
+
+      options.content.css({ 'left': options.position_left + 'px', 'top': options.position_top + 'px', 'display': 'none', 'visibility': 'visible' }).show();
+
+      options.afterOpen.apply(this, [options.content])
+
+
+      var num = 0
+      // You could add an active_popover class to $(this)
+      $(document).bind('click.popover', function(e) {
+        num++;
+        if (num > 1) {
+          $(options.content).removeClass('popover_container topleft bottomleft topright bottomright').hide();
+          options.afterClose.apply(this, [options.content])
+          $(document).unbind('click.popover');
+        }
+      });
+
+      //$('body').bind('click', function(e) {
+      //if($(e.target).closest('#menu').length == 0) {
+      // click happened outside of menu, hide any visible menu items
+      //}
+      //});
+
+
+    },
+    _open: function (options) {
+      options.beforeOpen();
+    },
+    close: function (options) {
+      options = $.extend({}, methods.defaults, options);
+      options.beforeClose();
+      options.afterClose();
+    },
+    _setupEvents: function (options) {
+      var _this = $(this)
+      if (options.reposition_on_resize) {
+        $(window).bind("reposition.popover", function () {
+          //_this.find('.modal').modal('position', options)
+        });
+      }
+    },
+    _positionArrow: function (options) {
+      var winTopMax = $(window).height() / 2,
+          winLeftMax = $(window).width() / 2,
+          pos = '';
+
+      (options.position_top >= 1 && options.position_top - $(window).scrollTop() <= winTopMax) ? pos = 'top' : pos = 'bottom';
+      (options.position_left >= 1 && options.position_left - $(window).scrollLeft() <= winLeftMax) ? pos += 'left' : pos += 'right';      
+      
+      return pos;
+    },
+    _positionPopover: function (options) {
+    }
+  };
+  $.fn.popover = function (method) {
+    if (methods[method]) {
+      return methods[ method ].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if (typeof method === 'object' || !method) {
+      return methods.init.apply(this, arguments);
+    } else {
+      $.error('Method ' + method + ' does not exist on expressionui.popover');
+    }
   }
 })(jQuery);
